ceb549c73afd79806eeed840443c79e569ac8f01
Nails V <nealdo@icloud.com>
2026-03-03 22:04:45 -0700

    learning love 2d a bit deleted some  stuff

---
+++ main.lua
@@ -0,0 +1,8 @@
+function love.load()
+    print("debug mode is on") --comment this out on realse
+    love.window.setMode(800,500)
+end
+
+function love.draw()
+    love.graphics.print("hello world", 500, 300)
+end
