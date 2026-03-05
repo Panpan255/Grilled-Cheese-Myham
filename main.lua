b8366c15fbdc429575dfbf9ef470922f4f48d612
Nails V <nealdo@icloud.com>
2026-03-04 20:41:52 -0700

    made a debug thing

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ function love.load()
     if screenWidth == 1280 and screenHeight == 720 then
         splashy.addSplash(love.graphics.newImage(images/widescreen/love-splash.png)) -- this loades splash screen for 720p(1280x720)
     elseif screenWidth == 800 and screenHeight == 500 then
+        print("screen set to 800 by 500") -- comment out later this is for debug
         splash.addSplash(love.graphics.newImage(images/Mini/love-splash.png)) -- this loades the splash for 800x500
     --elseif screenWidth == 1920 and screen height == 1080 then
         --splash.addSplash(love.graphics.newImage(images/HD/love-splash.png)) -- splash for 1920 by 1080
