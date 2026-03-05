aeee4919f7ef70f1d612d085ad1eb6fa1a12ca55
Gamerpanda255 <nealdo@icloud.com>
2026-03-05 10:19:56 -0700

    setup part of the conf for the game

--- conf.lua
+++ conf.lua
@@ -1,4 +1,10 @@
 -- this is the config file for the game
 
 function love.conf(t)
+    t.title     = "Griled Cheese game"
+    t.author    = "Gamerpanda255"
+    t.ur        = nil --put to the itch link later
+
+    t.identity  = nil --set to the save diretory
+    t.version   = "0.0.1"
 end
