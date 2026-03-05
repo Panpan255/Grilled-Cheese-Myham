d5d3066bada39eb4a10fb2e8617f65bbb1496c02
Gamerpanda255 <nealdo@icloud.com>
2026-03-05 10:22:55 -0700

    added in the window setup part in conf

--- conf.lua
+++ conf.lua
@@ -10,5 +10,7 @@ function love.conf(t)
     t.console   = true -- Set to false before export
     t.realse    = false --Enables realse mode
 
-
+    t.window.width  = 800
+    t.window.height = 500
+    t.window.fullscreen = false
 end
