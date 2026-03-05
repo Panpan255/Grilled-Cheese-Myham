470dccbcd8fbecb3a2bd835cacd68f2fc9ad647c
Nails V <nealdo@icloud.com>
2026-03-04 17:56:46 -0700

    added a test splash screen and setup the test

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
-function love.load()
+splashy = require 'libs/splashy'
+
+unction love.load()
     print("debug mode is on") --comment this out on realse
-    
+    splashy.addSplash(love.graphics.newImage("images/test-splash.png"))
     love.window.setMode(800,500)
 end
 
