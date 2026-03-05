e049b3aff110a33f50e8296765546c61858fc8a0
Gamerpanda255 <nealdo@icloud.com>
2026-03-05 10:06:24 -0700

    commented stuff out for testing

--- main.lua
+++ main.lua
@@ -8,16 +8,17 @@ function love.load()
     print("debug mode is on") --comment this out on realse
     screenWidth, screenHeight = love.graphics.getDimensions()
     print ("screen size", screenWidth, screenHeight) -- comment out this is debug
-    if screenWidth == 1280 and screenHeight == 720 then
-        splashy.addSplash(love.graphics.newImage(images/widescreen/love-splash.png)) -- this loades splash screen for 720p(1280x720)
-        print("splash screen for Widescreen is setup") -- comment out later this is for debug
-    elseif screenWidth == 800 and screenHeight == 500 then
-        print("splash screen for Mini setup") -- comment out later this is for debug
-        splash.addSplash(love.graphics.newImage(images/Mini/love-splash.png)) -- this loades the splash for 800x500
-    elseif screenWidth == 1920 and screenHeight == 1080 then
-        splash.addSplash(love.graphics.newImage(images/HD/love-splash.png)) -- splash for 1920 by 1080
-        print("slpash screen for HD is setup") -- comment out later this is for debug
-    end
+    --if screenWidth == 1280 and screenHeight == 720 then
+      --  splashy.addSplash(love.graphics.newImage(images/widescreen/love-splash.png)) -- this loades splash screen for 720p(1280x720)
+        --print("splash screen for Widescreen is setup") -- comment out later this is for debug
+    --elseif screenWidth == 800 and screenHeight == 500 then
+      --  print("splash screen for Mini setup") -- comment out later this is for debug
+       -- splash.addSplash(love.graphics.newImage(images/Mini/love-splash.png)) -- this loades the splash for 800x500
+    --elseif screenWidth == 1920 and screenHeight == 1080 then
+      --  splash.addSplash(love.graphics.newImage(images/HD/love-splash.png)) -- splash for 1920 by 1080
+       -- print("slpash screen for HD is setup") -- comment out later this is for debug
+    --end
+    splashy.addSplash(love.graphics.newImage(images/Mini/love-splash.png))
 
 
     --love.window.setMode(800,500) -- this sets the screen size to 800 by 500
