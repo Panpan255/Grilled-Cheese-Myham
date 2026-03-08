296d79e620c2100ad54ffc6c46c91746766cd639
Nails V <nealdo@icloud.com>
2026-03-07 22:33:43 -0700

    found a spelling mistake testing again

--- main.lua
+++ main.lua
@@ -35,7 +35,7 @@ function love.load()
     --choicing splash screen based on screen size
     if screenSetting == "Widescreen" then
         splashy.addSplash(love.graphics.newImage("images/widescreen/love-splash.png")) -- this loades splash screen for 720p(1280x720)
-        spashy.addSplash(love.graphics.newImage("images/widescreen/game-jam-splash.png"))
+        splashy.addSplash(love.graphics.newImage("images/widescreen/game-jam-splash.png"))
         print("splash screen for Widescreen is setup") -- comment out later this is for debug
     elseif screenSetting == "Mini" then
         print("splash screen for Mini setup") -- comment out later this is for debug
