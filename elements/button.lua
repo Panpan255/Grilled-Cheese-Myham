86dfdc98d731da4220ba6da14abab88170e1f78f
Nails V <nealdo@icloud.com>
2026-03-09 20:14:34 -0600

    made the window change also reset the game to reload all the textures

--- elements/button.lua
+++ elements/button.lua
@@ -37,10 +37,13 @@ function buttons.windowSize(x, y, windowSize)
     func = function(windowSize)
     if windowSize == "HD" then
         love.window.setMode(1280, 720)
+        resetGame()
     elseif windowSize == "widescreen"
         love.window.setMode(800, 500)
+        resetGame()
     elseif windowSize == "Mini" 
         love.window.setMode(1920, 1080)
+        resetGame()
     end
 
 
