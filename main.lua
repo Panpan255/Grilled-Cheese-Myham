16e3ad9fbe1e059c7301b08abcfcc2a95e5b9d64
Nails V <nealdo@icloud.com>
2026-03-21 15:14:41 -0600

    set  the menu setting to main by default

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ items = {
 }
 
 --what screen
-local menuSetting = ""
+local menuSetting = "main"
 
 
 -- screen settings varables
@@ -65,7 +65,7 @@ function love.update(dt)
         if screenSetting == "mini" then
         elseif screenSetting == "HD" then
         elseif screenSetting == "widescreen"then 
-            
+
     elseif menuSetting == "settings" then
         if screenSetting == "mini" then
         elseif screenSetting == "HD" then
