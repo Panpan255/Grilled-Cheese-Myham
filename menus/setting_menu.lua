79502a91be884f8b503252065c84dc110fc71312
Nails V <nealdo@icloud.com>
2026-03-20 22:00:55 -0600

    created all the functions for the printing screen functions

--- menus/setting_menu.lua
+++ menus/setting_menu.lua
@@ -1,3 +1,13 @@
 buttons = require "elements/button.lua"
 local love = require "love"
 
+local settingMenu = {}
+
+function settingMenu.print.HD()
+end
+
+function settingMenu.print.mini()
+end
+
+function settingMenu.print.widescreen()
+end
