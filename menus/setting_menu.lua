a2161d16b0f041d7715fd8ce44e55bba326f0eca
Nails V <nealdo@icloud.com>
2026-03-20 21:58:11 -0600

    realizing that I can just make functions for the screen sizes to use for printing

--- menus/setting_menu.lua
+++ menus/setting_menu.lua
@@ -1,21 +1,3 @@
 buttons = require "elements/button.lua"
 local love = require "love"
-screenSetting = require "screensetup"
 
-
-function printSettingsMenu()
-    
-    func = function()
-        local ScreenSize = screen.getScreenSize()
-
-        if ScreenSize == "Mini" then
-
-        elseif ScreenSize == "Widescreen" then
-
-        elseif ScreenSize == "HD" then
-            
-        end
-    end
-
-    
-end
