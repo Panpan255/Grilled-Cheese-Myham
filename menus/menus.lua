77caa65ec3b14269ee0ce7150bb0d28164e3914e
Nails V <nealdo@icloud.com>
2026-03-22 10:48:17 -0600

    coppied over code

--- menus/menus.lua
+++ menus/menus.lua
@@ -31,3 +31,17 @@ end
 -- Settings Menu
 
 local settingsMenu = {}
+
+function settingMenu.print.HD()
+    love.graphics.setBackgroundColor(109, 176, 165, 1.0)
+end
+
+function settingMenu.print.mini()
+    love.graphics.setBackgroundColor(109, 176, 165, 1.0)
+    
+end
+
+function settingMenu.print.widescreen()
+    love.graphics.setBackgroundColor(109, 176, 165, 1.0)
+   
+end
