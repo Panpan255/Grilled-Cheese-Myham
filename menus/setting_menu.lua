08db60a4d0ba95ebf45a84920b2beaab2598fda9
Nails V <nealdo@icloud.com>
2026-03-22 01:19:57 -0600

    realized its not for this type of function but for buttons that do things like change menu type

--- menus/setting_menu.lua
+++ menus/setting_menu.lua
@@ -5,15 +5,14 @@ local settingMenu = {}
 
 function settingMenu.print.HD()
     love.graphics.setBackgroundColor(109, 176, 165, 1.0)
-    love.update(dt)
 end
 
 function settingMenu.print.mini()
     love.graphics.setBackgroundColor(109, 176, 165, 1.0)
-    love.update(dt)
+    
 end
 
 function settingMenu.print.widescreen()
     love.graphics.setBackgroundColor(109, 176, 165, 1.0)
-    love.update(dt)
+   
 end
