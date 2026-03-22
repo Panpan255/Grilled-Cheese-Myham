effa3b7b6b68893c34daa4cdbe1eea2b5b3b3eec
Nails V <nealdo@icloud.com>
2026-03-22 01:10:06 -0600

    found the code for setting background colour

--- menus/main_main.lua
+++ menus/main_main.lua
@@ -5,6 +5,7 @@ buttons = require "elements/buttons"
 local mainMenu = {}
 
 function mainMenu.print.HD()
+    love.graphics.setBackgroundColor(106, 230, 157, 1.0))
 end
 
 function mainMenu.print.mini()
