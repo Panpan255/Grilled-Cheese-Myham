60c8176bef2bba7c872e742be078aefbed2c4d25
Gamerpanda255 <nealdo@icloud.com>
2026-03-23 20:52:01 -0600

    needed to move some stuff to be in right legth of line thing I forgot what it's called

--- screensetup.lua
+++ screensetup.lua
@@ -3,22 +3,21 @@ local screenSetting = "new"
 
 
 function screenSizeSet()
-
-func = function()
+    func = function()
     -- checking screen size 
-    screenWidth, screenHeight = love.graphics.getDimensions()
+        screenWidth, screenHeight = love.graphics.getDimensions()
     
     -- Changing screen size setting 
-    if screenWidth == 800 and screenHeight == 500 then
-        screenSetting = "Mini"
-        print("screenSetting set to mini") --Debug comment out after
-    elseif screenWidth == 1280 and screenHeight == 720 then
-        screenSetting = "Widescreen"
-        print("screenSetting set to Widescreen") --Debug comment out after
-    elseif screenWidth == 1920 and screenHeight == 1080 then
-        screenSetting = "HD"
-        print("screenSetting set to HD") --Debug comment out after
-    end
+        if screenWidth == 800 and screenHeight == 500 then
+            screenSetting = "Mini"
+            print("screenSetting set to mini") --Debug comment out after
+        elseif screenWidth == 1280 and screenHeight == 720 then
+            screenSetting = "Widescreen"
+            print("screenSetting set to Widescreen") --Debug comment out after
+        elseif screenWidth == 1920 and screenHeight == 1080 then
+            screenSetting = "HD"
+            print("screenSetting set to HD") --Debug comment out after
+        end
 end
 
 function getScreenSize()
