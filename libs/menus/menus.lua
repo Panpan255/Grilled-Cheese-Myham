24372745f6ed1186bda77f69d71e4abb2885285a
Gamerpanda255 <nealdo@icloud.com>
2026-03-30 11:41:51 -0600

    copied the button placement over to the other print settings

--- libs/menus/menus.lua
+++ libs/menus/menus.lua
@@ -45,11 +45,13 @@ local settingsMenu = {
 function settingsMenu.print.HD()
     love.graphics.setBackgroundColor(109, 176, 165, 1.0)
     --love.graphics.draw(mainMenuButton 100, 700)
+    buttons.homeMenu(100,700)
 end
 
 function settingsMenu.print.mini()
     love.graphics.setBackgroundColor(109, 176, 165, 1.0)
     --love.graphics.draw(mainMenuButton 100, 700)
+    buttons.homeMenu(100, 700)
     
 end
 
